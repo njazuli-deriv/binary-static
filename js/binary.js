65be585a430805e6ae8f22c876983a3e0a7782f7
njazuli-deriv <78890281+njazuli-deriv@users.noreply.github.com>
2021-03-15 10:23:39 +0800

    Deploy to gh-pages

--- js/binary.js
+++ js/binary.js
@@ -38056,7 +38056,7 @@ var binary_desktop_app_id = 14473;
 
 var getAppId = function getAppId() {
     var app_id = null;
-    var user_app_id = '24426'; // you can insert Application ID of your registered application here
+    var user_app_id = '24605'; // you can insert Application ID of your registered application here
     var config_app_id = window.localStorage.getItem('config.app_id');
     var is_new_app = /\/app\//.test(window.location.pathname);
     if (config_app_id) {
